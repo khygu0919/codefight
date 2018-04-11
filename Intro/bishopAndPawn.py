2f626a58c1a1a2d62b8985b6f29ede070e8a7228
khygu0919 <khyku0919@gmail.com>
2018-04-11 17:03:07 +0900

    수정
    저장안함

--- Intro/bishopAndPawn.py
+++ Intro/bishopAndPawn.py
@@ -7,15 +7,3 @@ def bishopAndPawn(bishop, pawn):
 	b=[ord(bishop[0]),int(bishop[1])]
 	p=[ord(pawn[0]),int(panw[1])]
 	return b[1]-b[0]==p[1]-p[0] or sum(b)==sum(p)
-print bishopAndPawn('e3','h3')
-'''
-a b c d e f g h
-1 2 3 4 5 6 7 8
-
-4,4
-1,1 2,2 3,3 5,5 6,6 7,7 8,8
-1,7 2,6 3,5 5,3 6,2 7,1
-5,3 6,4
-3,1 4,2 6,4 7,5 8,6
-1,7 2,6 3,5 4,4 6,2 7,1
-'''
