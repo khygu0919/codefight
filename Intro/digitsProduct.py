2c4e74621741992e0f696a14cf175e16b3bea438
khygu0919 <khyku0919@gmail.com>
2018-04-23 12:28:49 +0900

    Intro
    인트로 푸는 중

--- Intro/digitsProduct.py
+++ Intro/digitsProduct.py
@@ -8,17 +8,33 @@ digitsProduct(product) = 26;
 For product = 19, the output should be
 digitsProduct(product) = -1.
 '''
+import math
 def getDivisor(product):
     for i in range(9,1,-1):
         if product%i==0:
             return product//i,i
+
 def digitsProduct(product):
     c=[]
     a=0
+    if product==1:
+        return 1
+    if product==0:
+        return 10
+    if product==5 or product==7:
+        return product
+    for i in range(int(math.sqrt(product)),1,-1):
+        if product%i==0:
+            break
+        if product%i!=0 and i==2:
+            return -1
     while 1:
         if product==1:
             break
-        a=getDivisor(product)[1]
-        product=getDivisor(product)[0]
-        c.append(a)
-    return ''.join(str(x) for x in sorted(c))
+        try:
+            a=getDivisor(product)[1]
+            product=getDivisor(product)[0]
+            c.append(a)
+        except:
+        	return -1
+    return int(''.join(str(x) for x in sorted(c)))
